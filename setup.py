55dfbf82bace60409cca009887e93f80cd440027
Alishaw99 <tariqham@gmail.com>
2024-02-09 16:58:01 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'xray',
+    version= '0.0.0',
+    author= 'Tariq',
+    author_email= 'tariqhammer@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
